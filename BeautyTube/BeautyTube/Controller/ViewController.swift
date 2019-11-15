45ff27a4b14008f55299b951718c4a6b17b727a3
SeunghoBaek <seungho.white@gmail.com>
2019-11-15 21:11:05 +0900

    MVC structure 1st commit

---
+++ BeautyTube/BeautyTube/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BeautyTube
+//
+//  Created by 백승호 on 2019/11/15.
+//  Copyright © 2019 Seungho Baek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BeautyTube/BeautyTube/Model/YoutubeManager.swift
@@ -0,0 +1,9 @@
+//
+//  YoutubeManager.swift
+//  BeautyTube
+//
+//  Created by 백승호 on 2019/11/15.
+//  Copyright © 2019 Seungho Baek. All rights reserved.
+//
+
+import Foundation
